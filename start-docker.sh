748a6fa9b9c418de7cdefabf18c467ecc53e1963
heihotech <heiho.tech@gmail.com>
2021-11-06 11:53:01 +0700

    docker prune

--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker stop be-region
+docker container prune -f
 docker image rm simrs/be-region:latest -f
 docker build -t simrs/be-region:latest .
 docker run -p 3000:3000 --restart=always --name be-region -d simrs/be-region:latest

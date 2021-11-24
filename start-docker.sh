930c739ab0fad57745806a235f1952fdf3302c93
heihotech <heiho.tech@gmail.com>
2021-11-24 11:24:38 +0700

    docker port change

--- start-docker.sh
+++ start-docker.sh
@@ -3,4 +3,4 @@ docker stop be-region
 docker container prune -f
 docker image rm simrs/be-region:latest -f
 docker build -t simrs/be-region:latest .
-docker run -p 3000:3000 --restart=always --name be-region -d simrs/be-region:latest
+docker run -p 8086:8086 --restart=always --name be-region -d simrs/be-region:latest
